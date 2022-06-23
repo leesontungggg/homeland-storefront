b6b1561286d7077ee9de80ece63e49a18271a3e3
Eden Lee <edenlee@Edens-MacBook-Pro.local>
2022-06-23 16:08:33 +0700

    show sold out when quantity = 0

--- src/pages/[handle].js
+++ src/pages/[handle].js
@@ -5,6 +5,7 @@ import Steps from "../components/steps"
 import { client } from "../utils/client"
 
 const ProductPage = ({ product, regions }) => {
+  const quantity = product.variants[0].inventory_quantity
   const [region, setRegion] = useState(regions?.find(reg => reg.name === 'VN') || null)
   const [country, setCountry] = useState(region?.countries?.[0].iso_2 || "")
 
@@ -26,12 +27,21 @@ const ProductPage = ({ product, regions }) => {
           <meta name="description" content={product?.description || ""} />
         </Head>
         <div className="min-w-full min-h-screen flex flex-col items-center justify-center bg-ui-light pt-20">
-          <Steps
-            product={product}
-            regions={regions}
-            region={region}
-            country={country}
-          />
+          {
+            quantity > 0 ? (
+              <Steps
+                product={product}
+                regions={regions}
+                region={region}
+                country={country}
+              />
+            ) : (
+              <div className="w-screen h-screen flex items-center justify-center -mt-20">
+                <p className="text-2xl">Sự kiện {product.title} đã hết vé. Mong gặp lại bạn trong những sự kiện khác.</p>
+              </div>
+            )
+          }
+
         </div>
       </Layout>
     </>
@@ -40,8 +50,6 @@ const ProductPage = ({ product, regions }) => {
 
 export async function getStaticPaths() {
   const { products } = await client.products.list()
-
-  console.log('products', products)
 
   const paths = products
     .map(product => ({
